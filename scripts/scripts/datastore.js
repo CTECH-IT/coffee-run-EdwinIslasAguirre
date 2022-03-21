34643a92d1ab013d9f2f1eb5e29de94d94e94774
Edwin Islas Aguirre <8241001@isd535.org>
2022-03-21 11:30:46 -0500

    not sure what im doing, part 1 of coffee run

---
+++ scripts/scripts/datastore.js
@@ -0,0 +1,14 @@
+(function (window) {
+    'use strict';
+
+    
+    let App = Window.App || {};
+
+    function DataStore() {
+        console.log('Running the DataStore Function...');
+    }
+
+    App.DataStore = DataStore;
+    window.App = App;
+    
+})(window);
