db60be92cccb3d9403e85fab77ee40194655d47a
Edwin Islas Aguirre <8241001@isd535.org>
2022-03-22 19:52:31 -0500

    committing and pushing, page 26 in part 1

--- scripts/trucks.js
+++ scripts/trucks.js
@@ -2,9 +2,20 @@
     'use strict';
 
     let App = window.App || {};
-    
-    function Truck() {
 
+    function Truck(truckId, db) {
+        this.truckId = truckId;
+        this.db = db;
+    }
+
+    Truck.prototype.createOrder = function (order) {
+        console.log('Adding order for' + order.emailAdress);
+        this.db.add(order.emailAdress, order);
+    }
+
+    Truck.prototype.delieverOrder = function (customerId) {
+        console.log('Delievering order for' + customerId);
+        this.db.remove(customerId);
     }
 
     App.Truck = Truck;
