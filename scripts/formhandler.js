a435fdc12eed9fee5c2dcada4e820e517a258194
Edwin Islas Aguirre <8241001@isd535.org>
2022-03-24 11:31:28 -0500

    page 7 out of 19 in part 3 of CR

--- scripts/formhandler.js
+++ scripts/formhandler.js
@@ -2,11 +2,20 @@
     'use strict';
 
     let App = window.App || {};
+    let $ = window.jQuery;
 
-    function FormHandler() {
-        // do stuff here
+    function FormHandler(selector) {
+        if(!selector) {
+            throw new Error('No selector provided!');
+        }
     }
 
+    // find the "selector" in the DOM using jQuery and assign it to this.formElement
+    this.$formElement = $(element);
+    if (this.$formElement.length == 0) {
+        throw new error('Could not find Element with selector:' + selector);
+    }
+    
     App.FormHandler = FormHandler;
     window.App = App;
 
