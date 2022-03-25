fdf9d78f7e95c906b6bd32118c693b8757c11a61
Edwin Islas Aguirre <8241001@isd535.org>
2022-03-25 17:33:01 -0500

    page 9 out of 19 in CR part 3

--- scripts/formhandler.js
+++ scripts/formhandler.js
@@ -11,9 +11,21 @@
     }
 
     // find the "selector" in the DOM using jQuery and assign it to this.formElement
-    this.$formElement = $(element);
+    this.$formElement = $(selector);
     if (this.$formElement.length == 0) {
-        throw new error('Could not find Element with selector:' + selector);
+        throw new error('Could not find Element with selector: ' + selector);
+    }
+
+
+    // Add an event handler for the Submit button
+    FormHandler.prototype,addSubmitHandler = function () {
+        console.log('Setting the submit handler for the form...');
+        this.$formElement.on('submit', function(event) {
+            event.preventDefault();
+
+            let data = $(this).serializeArray();
+            console.log(data);
+        });
     }
     
     App.FormHandler = FormHandler;
