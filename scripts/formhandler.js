e589367d0e010c103a4da2d2cef9f2f2f0538a5a
Edwin Islas Aguirre <8241001@isd535.org>
2022-03-26 15:42:46 -0500

    finished with part 3 onto part 4

--- scripts/formhandler.js
+++ scripts/formhandler.js
@@ -5,29 +5,37 @@
     let $ = window.jQuery;
 
     function FormHandler(selector) {
-        if(!selector) {
+        if (!selector) {
             throw new Error('No selector provided!');
         }
-    }
 
     // find the "selector" in the DOM using jQuery and assign it to this.formElement
     this.$formElement = $(selector);
     if (this.$formElement.length == 0) {
         throw new error('Could not find Element with selector: ' + selector);
     }
+}
 
-
-    // Add an event handler for the Submit button
-    FormHandler.prototype,addSubmitHandler = function () {
+    // Add an event handler for the Submit button and pass in createOrder as a parameter (func)
+    FormHandler.prototype.addSubmitHandler = function (func) {
         console.log('Setting the submit handler for the form...');
         this.$formElement.on('submit', function(event) {
             event.preventDefault();
 
-            let data = $(this).serializeArray();
+            // copy all the info from the form fields into the variable called data
+            let data = {};
+            $(this).serializeArray().forEach(function (item) {
+                data[item.name] = item.value;
+                console.log(item.name + ' is ' + item.value);
+            });
             console.log(data);
+            func(data); // call the function that was passed in on the data from the form
+
+            this.reset(); // reset the form
+            this.elements[0].focus(); // focus on the first field
         });
-    }
-    
+    };
+
     App.FormHandler = FormHandler;
     window.App = App;
 
