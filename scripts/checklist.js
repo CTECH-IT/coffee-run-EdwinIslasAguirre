77331d726529b82f7937b5af2f1323356c479e85
Edwin Islas Aguirre <8241001@isd535.org>
2022-03-26 16:47:02 -0500

    pg 11/21 in part 4 of CR

---
+++ scripts/checklist.js
@@ -0,0 +1,57 @@
+(function (window) {
+    'use strict';
+    let App = window.App || {};
+    let $ = window.jQuery;
+
+    // the CheckList object constructor
+    function CheckList(selector) {
+        if (!selector) {
+            throw new error('No selector provided');
+        }
+        this.$element = $(selector);
+        if (this.$element.length === 0) {
+            throw new Error('Could not find element with selector: ' + selector);
+        }
+
+        // the method that adds a new row to the Checklist
+        CheckList.prototype.addRow = function (coffeeOrder) {
+            // Create a new instance of a row, using the coffee order info
+            var rowElement = new Row(coffeeOrder);
+            // Add the new row instance's $element property to the checklist 
+            this.$element.append(rowElement.$element);
+        };
+
+        // Each row is one outstanding order
+        function Row(coffeeOrder) {
+            // Constructor code will go here
+            let $div = $('<div></div>', {
+                'data-coffee-order': 'checkbox',
+                'class': 'checkbox'
+            });
+            let $label = $('<label></label>');
+
+            let $checkbox = $('<input></input>', {
+                type: 'checkbox',
+                value: coffeeOrder.emailAddress
+            })
+        }
+    }
+
+    let description = coffeeOrder.size + '';
+    if (coffeeOrder.flavor) {
+        description += coffeeOrder.flavor + '';
+    }
+    description += coffeeOrder.coffee + '';
+    description += '(' + coffeeOrder.emailAddress + ')';
+    description += '[' + coffeeOrder.strength + 'x]';
+
+    $label.append($checkbox);
+    $label.append(description);
+    $div.append($label);
+
+    this.$element = $div;
+
+    // Add the Checklist to the App namespace
+    App.CheckList = CheckList;
+    window.App = App;
+})(window);
