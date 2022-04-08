7ef719973180c9631e8a25edc467b01c23de0fae
Edwin Islas Aguirre <8241001@isd535.org>
2022-04-07 22:30:57 -0500

    check problem later

--- scripts/main.js
+++ scripts/main.js
@@ -10,6 +10,7 @@
     let DataStore = App.DataStore;
     let FormHandler = App.FormHandler;
     let CheckList = App.CheckList;
+    let Validation = App.Validation;
 
     let myTruck = new Truck('12345', new DataStore());
     window.myTruck = myTruck;
@@ -17,11 +18,14 @@
     let checkList = new CheckList(CHECKLIST_SELECTOR);
 
     let formHandler = new FormHandler(FORM_SELECTOR);
-    
+
     // when the submit button is called, create the order and add a checkbox
     formHandler.addSubmitHandler(function (data) {
         myTruck.createOrder.call(myTruck, data);
         CheckList.addRow.call(CheckList, data);
     });
-    
+
+    // add the email validator to the email input field
+    formHandler.addInputHandler(Validation.isCompanyEmail);
+
 })(window);
