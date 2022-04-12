d1b8e96092fe03d5b96b1c722f9c57a6e51ca358
Edwin Islas Aguirre <8241001@isd535.org>
2022-04-12 11:31:02 -0500

    almost done with coffeeRun in general

--- scripts/remotedatastore.js
+++ scripts/remotedatastore.js
@@ -1,7 +1,7 @@
 (function (window) {
     'use strict';
     var App = window.App || {};
-    var $ = window,jQuery;
+    var $ = window.jQuery;
     function RemoteDataStore(url) {
         if (!url) {
             throw new error('No remote URL supplied.');
@@ -17,7 +17,26 @@
         });
     };
 
+    RemoteDataStore.prototype.getAll = function (cb) {
+        // make a "get" call to the server URL
+        // pass in an anonymous function that calls the "cb" callback function
+        $.get(this.serverUrl, function (serverResponse) {
+            console.log(serverResponse);
+            cb(serverResponse)
+        });
+    };
+
+    RemoteDataStore.prototype.get = function (key, cb) {
+        // make a get call to the server, but pass an email address
+        // so that just one order
+        // then call the function "cb" on the response
+        $.get(this.serverUrl, + '?emailAddress=' + key, function (serverResponse) {
+            console.log(serverResponse);
+            cb(serverResponse);
+        });
+    };
+
     App.RemoteDataStore = RemoteDataStore;
-    window.App = App
-    
+    window.App = App;
+
 })(window);
